f88ddc3e937066ff86033e3db64d6602a33fb348
Simran <officialsimranahuja@gmail.com>
2023-11-04 15:02:02 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// let getIPBtn = document.querySelector("#getIPBtn");
+// console.log(getIPBtn);
+// getIPBtn.addEventListener("click" ,()=>{
+//     // alert("hey");
+//    console.log("clicked")
+// })
+// console.log("outside");
+
+
+function getStartedFun(){
+   alert("Hey User ! Do You really want to share IP Address with Us?")
+}
